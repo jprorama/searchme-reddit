91ec0767418729e8a2cb99e008455caa0f3149da
John-Paul Robinson <jprorama@gmail.com>
2019-11-14 06:15:05 -0600

    Add arg to print output as json objects
    Create dictionary to hold each entry and then print json as each
    output line.

--- showerthoughts.py
+++ showerthoughts.py
@@ -7,6 +7,7 @@ import logging
 import argparse
 import re
 from datetime import datetime, timezone
+import json
 
 def list_saved(listing):
     for entry in listing:
@@ -18,6 +19,8 @@ def list_shower(listing, subreddits):
     pattern = '|'.join(subreddits)
         
     for entry in listing:
+        sub = dict()
+
         if hasattr(entry, 'title'):
             title=entry.title
         else:
@@ -28,8 +31,12 @@ def list_shower(listing, subreddits):
             uri = "https://reddit.com{}".format(entry.permalink)
 
         utc = datetime.fromtimestamp(entry.created_utc, timezone.utc)
-            
+
+        sub = dict(title=title , id=entry.id, utc=entry.created_utc, subreddit=entry.subreddit_name_prefixed, ups=entry.ups, url=uri) 
         if re.search(pattern, entry.subreddit_name_prefixed, re.IGNORECASE):
+            if args.json:
+                print(json.dumps(sub))
+            else:
                  print("{}|{}|{}|{}|{}|{}".format(entry.ups, entry.id, utc, entry.subreddit_name_prefixed, title, uri))
 
 # collect list of search strings
@@ -38,6 +45,8 @@ parser.add_argument('-s','--subreddit', nargs='*',
                    help='case-insensitive pattern to match subreddit names')
 parser.add_argument('-a', '--api',
                     help='reddit api that returns a listing of submissions')
+parser.add_argument('-j', '--json', action="store_true",
+                    help='format output as json object')
 parser.add_argument('-d', '--debug', action='store_true',
                     help='turn on debug of network connection')
 
